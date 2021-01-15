5a4a90563b8e2c605dba5427c5e109bef1d8e775
deki90to <deki90to@gmail.com>
2021-01-15 01:44:48 +0100

    productname page

--- shop_project/shop_app/urls.py
+++ shop_project/shop_app/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('category/', views.CategoryListView.as_view(), name='category'),
     path('brand/', views.BrandListView.as_view(), name='brand'),
+    path('product_name/', views.DeviceListView.as_view(), name='device'),
 ]
--- shop_project/shop_app/views.py
+++ shop_project/shop_app/views.py
@@ -1,6 +1,6 @@
 from django.views import generic
 from django.views.generic.base import TemplateResponseMixin
-from . models import Category, Brand
+from . models import Category, Brand, ProductName
 from django.shortcuts import render
 
 class CategoryListView(generic.ListView):
@@ -10,3 +10,7 @@ class CategoryListView(generic.ListView):
 class BrandListView(generic.ListView):
     model = Brand
     template_name = 'brand_list.html'
+
+class DeviceListView(generic.ListView):
+	model = ProductName
+	template_name = 'productname_list.html'
