63af95808ccda58f5834bcdc7a151073da5e0945
deki90to <deki90to@gmail.com>
2021-01-19 01:45:11 +0100

    fixed log/reg form

--- shop_project/shop_app/urls.py
+++ shop_project/shop_app/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('product_name/', views.DeviceListView.as_view(), name='product'),
     path('buyer/', views.BuyerListView.as_view(), name='buyer'),
 
-
     path('category/createview/', views.CategoryCreateView.as_view(), name='category-createview'),
     path('brand/createview/', views.BrandCreateView.as_view(), name='brand-createview'),
     path('productname/createview/', views.ProductNameCreateView.as_view(), name='productname-createview'),
