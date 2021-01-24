b49a80b290bf4f6a95c569ad0538a3368924c330
deki90to <deki90to@gmail.com>
2021-01-24 16:29:22 +0100

    added price

--- shop_project/shop_app/models.py
+++ shop_project/shop_app/models.py
@@ -37,11 +37,11 @@ class ProductName(models.Model):
     date_posted = models.DateTimeField(auto_now_add=True)
     product_picture = ResizedImageField(size=[200,110], quality=100, upload_to='pictures', null=True, blank=True)
     product_brand = models.ForeignKey('Brand', on_delete=models.SET_NULL, null=True)
-    # product_price = models.IntegerField()
+    product_price = models.CharField(max_length=7, default='', null=True, blank=True)
     buyer = models.ForeignKey('Buyer', on_delete=models.SET_NULL, null=True, blank=True, related_name='buyer')
 
     def __str__(self):
-        return (f'Name:{self.product_name}')
+        return (f'{self.product_name}, {self.product_price} (EU)')
 
     def get_absolute_url(self):
         return reverse('product')
@@ -55,7 +55,7 @@ class Buyer(models.Model):
     address = models.TextField(max_length=500)
     note = models.TextField(max_length=1000)
     date_buyed = models.DateTimeField(auto_now_add=True)
-    # sold_product = models.ForeignKey('ProductName', on_delete=models.SET_NULL, null=True, blank=True, related_name='sold_product')
+    # buyed = models.ForeignKey('ProductName', on_delete=models.SET_NULL, null=True, blank=True, related_name='buyed')
 
 
     def __str__(self):
